2aa788dadc53e4b15586e282f4abdc1ca59ba00c
Vikram Hegde <vikhegde@gmail.com>
2024-01-18 22:21:56 -0800

    Completed Homework 7

---
+++ lesson-7/homework7/exercise-2.lua
@@ -0,0 +1,1 @@
+print("Curently a function is just a sequence of statements. A statement cannot be a function invocation so functions cant be nested. So no recursion direct or indirect is possible")
